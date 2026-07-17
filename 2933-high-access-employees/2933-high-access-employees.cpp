1b6958c6876e9f9c37fffe39b05a93b73f6a575f
Nitin Sharma <nitinsharmaop828@gmail.com>
2026-07-17 13:14:03 +0530

    Time: 51 ms (82.84%), Space: 52 MB (37.28%) - LeetHub

---
+++ 2933-high-access-employees/2933-high-access-employees.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+           int toMinutes(string s){
+              int HH= stoi(s.substr(0,2));
+              int MM=stoi(s.substr(2,2));
+              return HH * 60 + MM;
+           }
+    vector<string> findHighAccessEmployees(vector<vector<string>>& access_times) {
+        vector<string> ans;
+        unordered_map<string , vector<string>> mpp;
+        for(int i=0; i<access_times.size(); i++){
+            mpp[access_times[i][0]].push_back(access_times[i][1]);
+        }
+
+        for(auto &it : mpp){
+            auto &s= it.second;
+            sort(s.begin(), s.end());
+            for(int i=2; i<s.size(); i++){
+                if(toMinutes(s[i]) - toMinutes(s[i-2]) < 60){
+                    ans.push_back(it.first);
+                    break;
+                }
+            }
+        }
+
+       
+        return ans;
+    }
+};
