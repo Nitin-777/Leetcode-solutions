51622468918a929b430d65973d6d4f57e4bbd66d
Nitin Sharma <nitinsharmaop828@gmail.com>
2026-07-21 17:36:49 +0530

    Time: 87 ms (41.44%), Space: 80.7 MB (37.39%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -7,21 +7,20 @@ public:
         for(int i=0; i<s.size(); i++) {
             if(s[i] == '1') one++;
         }
-
         int i=0;
         while( i < s.size()){
             if(s[i] == '0'){
               int start=i;
               while(i < s.size() && s[i] == '0') i++;
               zero.push_back( i - start);
-              
             }
+            else{
             i++;
+            }
         }
           int maxi=0;
        for(int i=1; i<zero.size() ; i++){
             maxi=max(maxi, (zero[i] + zero[i-1]));
-
        }
        return maxi + one;
     }
