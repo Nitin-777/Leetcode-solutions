fc73b3e109e1254603804951c1413ec413362865
Nitin Sharma <nitinsharmaop828@gmail.com>
2026-07-21 17:34:38 +0530

    Time: 108 ms (27.03%), Space: 80.8 MB (37.39%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        vector<int>zero;
+        int one=0;
+        int cnt=0;
+        for(int i=0; i<s.size(); i++) {
+            if(s[i] == '1') one++;
+        }
+
+        int i=0;
+        while( i < s.size()){
+            if(s[i] == '0'){
+              int start=i;
+              while(i < s.size() && s[i] == '0') i++;
+              zero.push_back( i - start);
+              
+            }
+            i++;
+        }
+          int maxi=0;
+       for(int i=1; i<zero.size() ; i++){
+            maxi=max(maxi, (zero[i] + zero[i-1]));
+
+       }
+       return maxi + one;
+    }
+
+};
