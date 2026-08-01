297ae0bb06bbeb1cf98876d61dea695d1ef72638
Nitin Sharma <nitinsharmaop828@gmail.com>
2026-08-01 12:48:43 +0530

    Time: 0 ms (100%), Space: 10.1 MB (52.91%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
             
-            int solve(int i, int j, vector<int>& nums){
+            int solve(int i, int j, vector<int>& nums,vector<vector<int>>&dp){
                 if(i > j) return 0 ;
                 if(i == j) return nums[i];
+                  
+                  if(dp[i][j] != -1) return dp[i][j];
+                int take_i= nums[i] + min(solve(i+2,j,nums,dp), solve(i+1, j-1, nums,dp));
+                int take_j= nums[j] + min(solve(i, j-2, nums,dp), solve(i+1, j-1,nums,dp));
 
-                int take_i= nums[i] + min(solve(i+2,j,nums), solve(i+1, j-1, nums));
-                int take_j= nums[j] + min(solve(i, j-2, nums), solve(i+1, j-1,nums));
-
-                return max(take_i , take_j);
+                return dp[i][j]=max(take_i , take_j);
             }
      
-
-
-    bool predictTheWinner(vector<int>& nums) {
+        bool predictTheWinner(vector<int>& nums) {
         int i=0, j=nums.size();
+        vector<vector<int>>dp(23, vector<int>(23,-1));
 
         int total=accumulate(nums.begin(), nums.end(),0);
 
-        int player_1=solve(i,j-1,nums);
+        int player_1=solve(i,j-1,nums,dp);
         int player_2=total-player_1;
 
         return (player_1 >= player_2);
