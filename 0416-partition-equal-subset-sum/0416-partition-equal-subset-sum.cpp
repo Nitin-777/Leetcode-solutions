2db22a2c3cc586010360efea3d4bb981eb43d34d
Nitin Sharma <nitinsharmaop828@gmail.com>
2026-09-02 23:56:51 +0530

    Time: 207 ms (9.8%), Space: 30.8 MB (50.86%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+            int s=0;
+           int t[201][20005];
+
+            bool solve(int n, int s, int sum, vector<int>& nums){
+                  
+                    if(n < 0 ) return false;
+
+                    if(s == sum) return  true;
+
+                    if(t[n][s] != -1) return t[n][s];
+
+                    bool take= solve(n-1, s+nums[n], sum, nums);
+                    bool notTake=solve(n-1, s, sum, nums);
+
+                    return t[n][s] = take || notTake;
+
+            }
+
+    bool canPartition(vector<int>& nums) {
+        
+        int sum=0;
+        int n=nums.size();
+        memset(t, -1, sizeof(t));
+        for(auto &num : nums){
+            sum+=num;
+        }
+
+        if(sum % 2 != 0) return false;
+        sum/=2;
+
+        return solve(n-1,s, sum, nums);
+    }
+};
