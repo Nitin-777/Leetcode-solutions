5c00c130fb0fe57da9d046cf2b7ef11838970f5c
Nitin Sharma <nitinsharmaop828@gmail.com>
2026-09-04 20:23:48 +0530

    Time: 36 ms (25.35%), Space: 54.3 MB (68.08%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public: 
-                const int MOD= 1e9 + 7;
-
-             
-
+              const int MOD= 1e9 + 7;
     int numSubseq(vector<int>& nums, int target) {
-       // vector<int>arr;
-        long long ans=0;
-        // solve(0,nums,ans,arr,target);
-        sort(nums.begin(),nums.end());
-        int i=0, j=nums.size()-1;
-        int n=nums.size();
-
-        vector<int>p(n);
-        p[0]=1;
-        for(int i=1; i<n; i++){
-            p[i]= (2LL* p[i-1]) % MOD;
-        }
-        
-
-        while(i <= j){
-            if(nums[i] + nums[j] <= target){
-                ans = (ans + p[j - i]) % MOD;
-                 i++;
+            int ans=0;
+            sort(nums.begin(),nums.end());
+            int n=nums.size();
+            vector<int>power(n,0);
+            power[0]=1;
+            for(int i=1; i<n; i++){
+                power[i]= (power[i-1] * 2) % MOD;
             }
-            else j--;
 
-        }
-         return ans;
+            int l=0, r=nums.size()-1;
+            while(l <= r){
+                if(nums[l] + nums[r] <= target){
+                    int diff=r-l;
+                    ans=(ans  % MOD + power[diff]) % MOD;
+
+                    l++;
+                }
+
+                else r--;
+                
+            }
+
+            return ans % MOD;
     }
 };
